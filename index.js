d1b1ea46221700f3c853b4892a1592894535b61a
Pawel Garkusha <kievlanservers@gmail.com>
2019-02-07 12:38:58 +0200

    lll

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ const TOKEN = '757109302:AAH8fJyA4MBNetHMP2nHVKGGgEzPwxmUrOA'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, `Hello, glad to see you on KAC Group, ${msg.from.first_name}"`)
+  bot.sendMessage(msg.chat.id, `Hello, glad to see you on KAC Group, ${msg.from.first_name}. Type "/info" to see information about company`)
+
+
+  require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+  })﻿
+
+  bot.on('/info', msg => {
+    const { chat: { id}} = msg
+    bot.sendMessage(id, 'KAC Group bla bla bla')
+  })
+
 })
